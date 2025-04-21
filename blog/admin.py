e69f6a17501d0f98a6d7dc869a61b8fdfe5eede8
joao-carlos-dev <joaocarlosifsc@hotmail.com>
2025-04-21 20:20:43 -0300

    ajustes no template

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+<<<<<<< Updated upstream
+admin.site.register(Post)
+=======
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "status", "created_on")
     list_filter = ("status",)
@@ -10,3 +13,4 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     
 admin.site.register(Post, PostAdmin)
+>>>>>>> Stashed changes
